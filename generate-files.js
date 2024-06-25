0d847295e751e25179b63f1a9097de7fa8bd9c3b
Okanlawon Aanuoluwapo <tunsabaze01@gmail.com>
2024-06-25 12:13:17 +0000

    Package json file updated

---
+++ generate-files.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const generateFiles = (dir, count) => {
+  for (let i = 0; i < count; i++) {
+    const filePath = path.join(dir, `file${i}.txt`);
+    fs.writeFileSync(filePath, `This is file number ${i}`);
+  }
+};
+
+const createDirectoriesAndFiles = () => {
+  const baseDir = path.join(__dirname, "public", "large_files");
+
+  if (!fs.existsSync(baseDir)) {
+    fs.mkdirSync(baseDir, { recursive: true });
+  }
+
+  generateFiles(baseDir, 3000);
+};
+
+createDirectoriesAndFiles();
+console.log("Files generated successfully.");
